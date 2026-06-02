a75a8d9e410340f6585141901b0a298a45e15374
AbdulRehmanNasar <abdulrehmannasirmohmood@gmail.com>
2026-06-02 13:08:52 +0500

    Initial commit.

---
+++ api/license/[licenseKey].js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+  const { licenseKey } = req.query;
+
+  const licenses = {
+    "JMW-001": {
+      active: true,
+      expiryDate: "2026-08-01"
+    }
+  };
+
+  const license = licenses[licenseKey];
+
+  if (!license) {
+    return res.status(404).json({
+      active: false,
+      expiryDate: null
+    });
+  }
+
+  return res.status(200).json(license);
+}
